ff17ac5f9c64ec8b956a247aa9e7f830d02d5bd8
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-06-13 20:46:41 +0900

    add core_controller

---
+++ scripts/core_controller.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+name = 'core_controller'
+
+import rospy
+
+import time
+import std_msgs.msg
+
+
+class controller(object):
+
+    def __init__(self):
+        self.ps = PS()
+        self.logger = LOGGER()
+
+
+class PS(object):
+
+    def __init__(self):
+        self.pub = {}
+        pass
+
+    def publish(self, topic_name, msg):
+        self.pub[topic_name].pulish(msg)
+        return
+
+    def set_publish(self, topic_name, data_class, queue_size, latch = True):
+        if topic_name not in self.pub:
+            self.pub[topic_name] = rospy.Publisher(topic_name = topic_name, data_class = data_class, queue_size = queue_size, latch = latch)
+            time.sleep(0.01)
+
+        else:
+            pass
+        return
+
+
+class LOGGER(object):
+
+    def __init__(self):
+        rospy.init_node(topic_name)
+        self.ps = PS()
+
+    def start(self, db_path):
+        topic_name = '/logger_path'
+
+        self.ps.set_publish(topic_name = topic_name, data_class = std_msgs.msg.String, queue_size = 1, latch = True)
+
+        self.ps.publish(topic_name = topic_name, msg = db_path)
+
+    def stop(self):
+        topic_name = '/logger_path'
+
+        self.ps.set_publish(topic_name = topic_name, data_class = std_msgs.msg.String, queue_size = 1, latch = True)
+
+        self.ps.publish(topic_name = topic_name, msg = '')
