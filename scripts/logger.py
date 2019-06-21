dd558f5ced25b8358ce8772570dd5412d3648160
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-06-21 23:51:45 +0900

    tset

--- scripts/logger.py
+++ scripts/logger.py
@@ -34,7 +34,7 @@ def compare_topic_list():
             topic_li = new_topic_li
             print("5")
             make_Subscriber()
-            pritnt(topic_li)
+            print(topic_li)
             print("make sub")
 
 
