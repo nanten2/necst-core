ca50ce8715b4cee305b1dafb60c93153405d4c04
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-06-21 22:23:17 +0900

    topic monitor

--- scripts/funclist.py
+++ scripts/funclist.py
@@ -1,16 +1,15 @@
 #!usr/bin/env python3
 
 import db_logger_operation
-#import topic_monitor
+import topic_monitor
 #import web_monitor
 
 db_logger_operation = db_logger_operation.db_logger_operation()
-#topic_monitor = topic_monitor.topic_monitor()
+topic_monitor = topic_monitor.topic_monitor()
 #web_monitor = web_monitor.web_monitor()
 
 def func_li(data):
     func_li = [db_logger_operation.regist(data),]
-               #topic_monitor.regist(),
+               topic_monitor.regist(),
                #web_monitor.web_monitor.regist()]
     return func_li
-
