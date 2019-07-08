98b533a9c46a7afbfbbe7d0213809b38d02b1838
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-07-08 20:37:12 +0900

    add

--- scripts/db_logger_always_vol2.py
+++ scripts/db_logger_always_vol2.py
@@ -16,10 +16,10 @@ class db_logger_always_vol2(object):
         self.db_path = '/home/exito/data/logger/test/20190708/kondo-always.db'
         self.db = necstdb.necstdb()
         self.th = threading.Thread(target= self.loop)
-        self.th1 = threading.Thread(target= self.make_temp_data)
+        #self.th1 = threading.Thread(target= self.make_temp_data)
 
         self.th.start()
-        self.th1.start()
+        #self.th1.start()
         pass
 
 
@@ -58,6 +58,27 @@ class db_logger_always_vol2(object):
     def loop(self):
 
         while True:
+            self.temp_data_list = []
+            st = time.time()
+            while time.time()-st < t:
+                d = self.data_list.pop(0)
+                topic = list(d["data"].keys())
+                data = d["data"][str(topic)]
+
+                list_num = len(temp_data_list)
+                list_topic = []
+                for i in range(list_num):
+                    s = list(temp_data_list[i].keys())
+                    list_topic.append(s[0])
+
+                for i in range(len(list_topic)):
+                    if topic not in list_topic:
+                        self.temp_data_list.append(d)
+                    else:
+                        index = list_topic.index(topic)
+                        self.temp_data_list[index]["data"][topic] = d[topic]
+                continue
+
             if len(self.temp_data_list) == 0:
                 self.db.finalize()
                 pass
@@ -71,7 +92,6 @@ class db_logger_always_vol2(object):
             # if os.path.exits(self.dbpath[:self.dbpath.rfind('/')]):pass
             # else: os.makedirs(self.dbpath[:self.dbpath.rfind('/')])
             # self.db =necstdb.necstdb(self.dbpath,len(data))
-            d_list = []
 
             self.db.insert(d)
             continue
