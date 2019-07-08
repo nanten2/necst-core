582650e2e3b39ce31e56896e06c0d33acca5f3f0
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-07-08 19:23:29 +0900

    test

--- scripts/funclist.py
+++ scripts/funclist.py
@@ -8,12 +8,15 @@ import db_logger_always
 db_logger_operation = db_logger_operation.db_logger_operation()
 topic_monitor = topic_monitor.topic_monitor()
 db_logger_always = db_logger_always.db_logger_always()
+db_logger_always_vol2 = db_logger_always.db_logger_always_vol2()
+
 #web_monitor = web_monitor.web_monitor()
 
 def func_li():
     func_li = [db_logger_operation.regist,
                topic_monitor.regist,
                db_logger_always.regist,
+               db_logger_always_vol2.regist,
                #web_monitor.web_monitor.regist,
                ]
     return func_li
