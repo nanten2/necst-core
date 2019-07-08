6395072dc4d7e8d39010172f76d707bc2d9836a7
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-08 14:37:20 +0900

    bug fix core-controller

--- scripts/funclist.py
+++ scripts/funclist.py
@@ -1,14 +1,17 @@
 #!usr/bin/env python3
 
+import db_logger_always
 import db_logger_operation
 import topic_monitor
 #import web_monitor
 
+db_logger_always = db_logger_always.db_logger_always()
 db_logger_operation = db_logger_operation.db_logger_operation()
 topic_monitor = topic_monitor.topic_monitor()
 #web_monitor = web_monitor.web_monitor()
 
 def func_li():
-    func_li = [db_logger_operation.regist,
+    func_li = [db_logger_always.regist,
+               db_logger_operation.regist,
                topic_monitor.regist]
-               #web_monitor.web_monitor.regist()]
+               #web_monitor.web_monitor.regist]
