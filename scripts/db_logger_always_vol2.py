736a0dc9b033a269a8f50f4093316d79e28903ad
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-07-08 19:21:14 +0900

    add vol2

---
+++ scripts/db_logger_always_vol2.py
@@ -0,0 +1,79 @@
+#!usr/bin/env python3
+
+name  = 'db_logger_operation'
+
+import time
+import threading
+import necstdb
+
+import rospy
+import std_msgs.msg
+
+class db_logger_operation(object):
+
+    def __init__(self):
+        self.temp_data_list = []
+        self.db_path = '/home/exito/data/logger/test/20190708/kondo-always.db'
+        self.db = necstdb.necstdb()
+        self.th = threading.Thread(target= self.loop)
+        self.th1 = threading.Thread(target= self.make_temp_data)
+
+        self.th.start()
+        self.th1.start()
+        pass
+
+
+    def callback_path(self, req):
+        self.db_path = req.data
+        return
+
+    def regist(self, data):
+        if self.db_path != '':
+            self.temp_data_list.append({'path': self.db_path, 'data': data})
+        else: pass
+
+        return
+
+    def make_temp_data(self,t=10):
+        while True
+            self.temp_data_list = []
+            st = time.time()
+            while time.time()-st < t:
+                d = self.data_list.pop(0)
+                topic = list(d.keys())
+                data = d[str(topic)]
+
+                list_num = len(temp_data_list)
+                list_topic = []
+                for i in range(list_num):
+                    s = list(a[i].keys())
+                    list_topic.append(s[0])
+
+                for i in range(len(list_topic)):
+                    if topic is not in list_topic:
+                        self.temp_data_list.append(d)
+                    else:
+                        index = list_topic.index(topic)
+                        self.temp_data_list[index][topic] = d[topic]
+
+    def loop(self):
+
+        while True:
+            if len(self.temp_data_list) == 0:
+                self.db.finalize()
+                pass
+
+                if rospy.is_shutdown():
+                    break
+                time.sleep(0.01)
+                continue
+
+            d = self.temp_data_list.pop(0)
+            # if os.path.exits(self.dbpath[:self.dbpath.rfind('/')]):pass
+            # else: os.makedirs(self.dbpath[:self.dbpath.rfind('/')])
+            # self.db =necstdb.necstdb(self.dbpath,len(data))
+            d_list = []
+
+            self.db.insert(d)
+            continue
+        return
