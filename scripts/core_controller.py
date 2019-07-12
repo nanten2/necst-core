6cedb532f3fdc16e023a838ed6806ff73ccde562
ogawalab <ogawalab@ogawalab-no-MacBook-Air.local>
2019-07-12 17:32:57 +0900

    test

--- scripts/core_controller.py
+++ scripts/core_controller.py
@@ -54,16 +54,3 @@ class logger(object):
 
         self.make_pub.publish(topic_name, data_class, msg = '')
         return
-
-
-class test(object):
-
-    def __init__(self):
-        self.make_pub = make_pub()
-
-    def msg_test(self, command):
-        topic_name = '/test_topic1'
-        data_class = std_msgs.msg.Float64
-
-        self.make_pub.publish(topic_name, data_class, msg = command)
-        return
