cf4bc3aa39d975a3f4dadc0a1482a50b3f5786d2
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2019-07-24 14:22:10 +0900

    fix

--- scripts/db_logger_always.py
+++ scripts/db_logger_always.py
@@ -3,6 +3,7 @@
 name  = 'db_logger_always'
 
 import time
+import datetime
 import threading
 import necstdb
 
@@ -12,20 +13,19 @@ import std_msgs.msg
 class db_logger_always(object):
 
     def __init__(self):
-        self.db_path = '/home/exito/data/logger/test/20190708/2130-always.db'
+        self.db_path = '/home/exito/data/logger/always/'
         self.last_append_time = 0
         self.current_topic_list = []
         self.data_list = []
-        
+        self.receive_time_dic ={}
+
         self.db = necstdb.necstdb()
         self.th = threading.Thread(target= self.loop)
         self.th.start()
         pass
 
     def regist(self, data):
-        if data['time'] -  self.last_append_time >= 10:
-            self.data_list.append({'path': self.db_path, 'data': data})
-            pass
+            self.data_list.append({'path': , 'data': data})
         return
 
     def loop(self):
@@ -40,13 +40,15 @@ class db_logger_always(object):
                 continue
             
             d = self.data_list.pop(0)
-               
-            if d['data']['topic'] not in self.current_topic_list:
+            d['path'] = self.db_path + "{0:%Y%m%d}.db".format(datetime.datetime.now())
+            
+            if d['data']['topic'] not in self.receive_time_dic:
+                self.receive_time_dic[d['data']['topic']] = d['data']['time']
                 self.db.insert(d)
-                self.current_topic_list.append(d['data']['topic'])
-            else:
-                self.last_append_time = d['data']['time']
-                self.current_topic_list = []
-                self.data_list = []
+                pass
+            
+            if self.receive_time_dic[d['data']['topic']] - d['data']['time'] >= 10:
+                self.db.insert(d)
+                self.receive_time_dic[d['data']['topic']] = d['data']['time']
             continue 
         return   
