30065a8fe24ad8ae9ddfdd70bdbd3751e0819922
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2019-08-08 13:54:29 +0900

    fix

--- scripts/db_logger_always.py
+++ scripts/db_logger_always.py
@@ -14,41 +14,131 @@ class db_logger_always(object):
 
     def __init__(self):
         self.db_path = '/home/exito/data/logger/always/'
-        self.last_append_time = 0
-        self.current_topic_list = []
+        self.db_path_data = ''
         self.data_list = []
         self.receive_time_dic ={}
+        self.table_dict = {}
 
-        self.db = necstdb.necstdb()
         self.th = threading.Thread(target= self.loop)
         self.th.start()
         pass
 
     def regist(self, data):
-        self.data_list.append({'path': "", 'data': data})
+        self.data_list.append(data)
+        return
+    
+    def close_tables(self):
+        tables = self.table_dict
+        self.table_dict = {}
+        [tables[name].close() for name in tables]   
         return
 
+    def db_date(self):
+        if self.db_path_data != "{0:%Y%m%d}".format(datetime.datetime.now()):
+            self.db_path_data = self."{0:%Y%m%d}".format(datetime.datetime.now())
+            slef.db = necstdb.opendb(self.db_path + self.db_path_data)
+            self.close_tables()
+            pass
+        return
+   
     def loop(self):
         while True:    
             if len(self.data_list) == 0:
-                self.db.finalize()
-                pass
-                    
+                self.close_tables()
                 if rospy.is_shutdown():
                     break
                 time.sleep(0.01)
                 continue
             
             d = self.data_list.pop(0)
-            d['path'] = self.db_path + "{0:%Y%m%d}.db".format(datetime.datetime.now())
-            
-            if d['data']['topic'] not in self.receive_time_dic:
-                self.receive_time_dic[d['data']['topic']] = d['data']['time']
-                self.db.insert(d)
+
+            if d['topic'] not in self.receive_time_dic:
+                self.receive_time_dic[d['topic']] = d['received_time']
                 pass
-            
-            if self.receive_time_dic[d['data']['topic']] - d['data']['time'] >= 10:
-                self.db.insert(d)
-                self.receive_time_dic[d['data']['topic']] = d['data']['time']
-            continue 
+
+            elif self.receive_time_dic[d['topic']] - d['received_time'] < 10:
+                continue   
+
+            else:
+                 self.receive_time_dic[d['topic']] = d['received_time'] 
+                 pass
+
+            self.db_data()
+
+            table_name = d['topic'].replace('/', '-')
+            table_data = [d['received_time']]
+            table_info = [{'key': 'timestamp',
+                           'format': 'd',
+                           'size': 8}]
+
+            for slot in d['slots']:
+                if slot['type'].startswith('bool'):
+                    info = {'format': 'c', 'size': 1}
+
+                elif slot['type'].startswith('byte'):
+                    info = {'format': 's', 'size': len(slot['value'])}
+
+                elif slot['type'].startswith('char'):
+                    info = {'format': 'c', 'size': 1}
+
+                elif slot['type'].startswith('float32'):
+                    info = {'format': 'f', 'size': 4}
+
+                elif slot['type'].startswith('float64'):
+                    info = {'format': 'd', 'size': 8}
+
+                elif slot['type'].startswith('int8'):
+                    info = {'format': 'b', 'size': 1}
+
+                elif slot['type'].startswith('int16'):
+                    info = {'format': 'h', 'size': 2}
+
+                elif slot['type'].startswith('int32'):
+                    info = {'format': 'i', 'size': 4}
+
+                elif slot['type'].startswith('int64'):
+                    info = {'format': 'q', 'size': 8}
+
+                elif slot['type'].startswith('string'):
+                    info = {'format': 's', 'size': len(slot['value'])}
+                    slot['value'] = slot['value'].encode()
+
+                elif slot['type'].startswith('uint8'):
+                    info = {'format': 'B', 'size': 1}
+
+                elif slot['type'].startswith('unit16'):
+                    info = {'format': 'H', 'size': 2}
+
+                elif slot['type'].startswith('unit32'):
+                    info = {'format': 'I', 'size': 4}
+
+                elif slot['type'].startswith('unit64'):
+                    info = {'format': 'Q', 'size': 8}
+                else:
+                    continue
+
+                if isinstance(slot['value'], tuple):
+                    # for MultiArray
+                    dlen = len(slot['value'])
+                    info['format'] = '{0:d}{1:s}'.format(dlen, info['format'])
+                    info['size'] *= dlen
+                    table_data += slot['value']
+                else:
+                    table_data += [slot['value']]
+                    pass
+
+                info['key'] = slot['key']
+                table_info.append(info)
+                continue
+
+            self.db.create_table(table_name,
+                            {'data': table_info,
+                             'memo': 'generated by db_logger_operation',
+                             'version': necstdb.__version__,})
+
+            if table_name not in self.table_dict:
+                self.table_dict[table_name] = self.db.open_table(table_name, mode='ab')
+                pass
+
+            self.table_dict[table_name].append(*table_data)
         return   
