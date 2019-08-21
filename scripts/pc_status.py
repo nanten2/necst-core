1d0636f8fca1099702fa4ede973ca380047db353
Nishimura, Atsushi <ars096@gmail.com>
2019-08-21 20:53:27 +0900

    change topic name

--- scripts/pc_status.py
+++ scripts/pc_status.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     rate_ = rospy.get_param('~rate', 0.1)
     rate = rospy.Rate(rate_)
     
-    cpu = rospy.Publisher(name+'/cpu', std_msgs.msg.Float32, queue_size=1, latch=True)
-    mem = rospy.Publisher(name+'/mem', std_msgs.msg.Float32, queue_size=1, latch=True)
-    disk = rospy.Publisher(name+'/disk', std_msgs.msg.Float32, queue_size=1, latch=True)
-    login = rospy.Publisher(name+'/login', std_msgs.msg.Int16, queue_size=1, latch=True)
-    proc = rospy.Publisher(name+'/proc', std_msgs.msg.Int16, queue_size=1, latch=True)
+    cpu = rospy.Publisher('/core/'+name+'/cpu', std_msgs.msg.Float32, queue_size=1, latch=True)
+    mem = rospy.Publisher('/core/'+name+'/mem', std_msgs.msg.Float32, queue_size=1, latch=True)
+    disk = rospy.Publisher('/core/'+name+'/disk', std_msgs.msg.Float32, queue_size=1, latch=True)
+    login = rospy.Publisher('/core/'+name+'/login', std_msgs.msg.Int16, queue_size=1, latch=True)
+    proc = rospy.Publisher('/core/'+name+'/proc', std_msgs.msg.Int16, queue_size=1, latch=True)
     
     def publish_if_changed(pub, last, new):
         if new != last:
