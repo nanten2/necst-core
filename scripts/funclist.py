7bf5e5a3d963dba3bd860a7aec32adf9d47fef54
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2020-12-04 15:20:44 +0900

    add tp_logger

--- scripts/funclist.py
+++ scripts/funclist.py
@@ -3,18 +3,20 @@
 import db_logger_operation
 import topic_monitor
 import db_logger_always
+import db_logger_tp
 #import web_monitor
 
 db_logger_operation = db_logger_operation.db_logger_operation()
 topic_monitor = topic_monitor.topic_monitor()
 db_logger_always = db_logger_always.db_logger_always()
-
+db_logger_tp = db_logger_tp.db_logger_tp()
 #web_monitor = web_monitor.web_monitor()
 
 def func_li():
     func_li = [db_logger_operation.regist,
                topic_monitor.regist,
                db_logger_always.regist,
+               db_logger_tp.regist,
                #web_monitor.web_monitor.regist,
                ]
     return func_li
