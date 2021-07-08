e581b205411e85acdc443fcdd09078d9e021ec70
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-07-08 11:26:49 +0900

    #1 refactor main logger script

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,52 +1,53 @@
 #!/usr/bin/env python3
 
-name = 'logger'
-
 import time
-import funclist
+from typing import List, Any, Type
 
 import rospy
-import std_msgs.msg
+import std_msgs.msg  # noqa: F401
 
-ignore_topics = [
-    '/rosout',
-    '/rosout_agg',
+from . import funclist
+
+NODE_NAME = "logger"
+
+IGNORE_TOPICS = [
+    "/rosout",
+    "/rosout_agg",
+]
+IGNORE_TYPES = [
+    "unknown type",
 ]
 
-ignore_types = [
-    'unknown type',
-]
 
-def get_current_topic_list():
+def get_current_topic_list() -> List[List[str, str]]:
     topic_list = rospy.get_published_topics()
-    for topic in topic_list[:]:
-        if topic[0] in ignore_topics:
+    for topic in topic_list:
+        if topic[0] in IGNORE_TOPICS:
             topic_list.remove(topic)
-        elif topic[1] in ignore_types:
+        elif topic[1] in IGNORE_TYPES:
             topic_list.remove(topic)
-            pass
-        continue
     return topic_list
 
-def make_subscriber(topic):
+
+def make_subscriber(topic: List[str, str]) -> None:
     topic_name = topic[0]
-    topic_type = eval(topic[1].replace('/', '.msg.'))
+    topic_type = eval(topic[1].replace("/", ".msg."))
     rospy.Subscriber(
-        name = topic_name,
-        data_class = topic_type,
-        callback = callback,
-        callback_args = topic_name,
-        queue_size = 1
+        name=topic_name,
+        data_class=topic_type,
+        callback=callback,
+        callback_args=topic_name,
+        queue_size=1,
     )
 
-def callback(req, arg):
-    slots = [{'key': key,
-              'type': type_,
-              'value': req.__getattribute__(key)}
-             for key, type_
-             in zip(req.__slots__, req._slot_types)]
 
-    data = {'topic': arg, 'received_time': time.time(), 'slots': slots}
+def callback(req: Type[Any], arg: str) -> None:
+    slots = [
+        {"key": key, "type": type_, "value": req.__getattribute__(key)}
+        for key, type_ in zip(req.__slots__, req._slot_types)
+    ]
+
+    data = {"topic": arg, "received_time": time.time(), "slots": slots}
 
     flist = funclist.func_li()
     for f in flist:
@@ -54,8 +55,8 @@ def callback(req, arg):
     return
 
 
-if __name__ == '__main__':
-    rospy.init_node(name)
+if __name__ == "__main__":
+    rospy.init_node(NODE_NAME)
 
     subscribing_topic_list = []
     while not rospy.is_shutdown():
@@ -64,7 +65,4 @@ if __name__ == '__main__':
             if topic not in subscribing_topic_list:
                 make_subscriber(topic)
                 subscribing_topic_list.append(topic)
-                pass
-            continue
         time.sleep(1)
-        continue
